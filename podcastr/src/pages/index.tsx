68101a3567c07c553055c3261fef2475f5e32c51
Felipe <felipe.unip.adm2015@gmail.com>
2021-04-20 23:44:45 -0300

    penúltimo commit aula 2

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -1,5 +1,17 @@
+//SPA
+//SSR
+//SSG
+
+import { useEffect } from "react"
+
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
   return (
-    <h1>Hello World</h1>
+    <h1>Index</h1>
   )
 }
