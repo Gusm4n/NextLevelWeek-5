9c126b3cba6aab5a45c722719f683f8ae8320c64
Felipe <felipe.unip.adm2015@gmail.com>
2021-04-21 16:14:38 -0300

    último commit aula 2

--- podcastr/src/pages/index.tsx
+++ podcastr/src/pages/index.tsx
@@ -2,16 +2,21 @@
 //SSR
 //SSG
 
-import { useEffect } from "react"
-
-
-export default function Home() {
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }, [])
+export default function Home(props) {
+console.log(props.episodes)
   return (
     <h1>Index</h1>
   )
+}
+
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+      props: {
+        episodes: data,
+      }
+    }
 }
